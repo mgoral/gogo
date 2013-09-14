b06853b3c2e20323a490a845423106bf2c5b6f3c
Michał Góral <virgoerns@gmail.com>
2013-09-14 21:20:09 +0200

    Fix displaying editor in a terminal.
    Unfortunately now the gogo function has to partially parse gogo arguments. Fortunately gogo allows
    only one arg at a time so it's not that bad.

--- gogo.sh
+++ gogo.sh
@@ -2,8 +2,13 @@
 # Gogo from anywhere.
 function gogo
 {
+    if [[ "$1" == "-e" ]] || [[ "$1" == "--editor" ]]; then
+        gogo.py $1
+        return
+    fi
+
     dir=$(gogo.py $1)
-    if [ $? -eq 0 ]; then
+    if [[ $? -eq 0 ]]; then
         cd "${dir}"
     fi
 }
